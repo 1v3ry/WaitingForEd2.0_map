00c099a0359a0c8574cd4c223b25077ba2998047
1v3ry <oliver.schoenefeld@me.com>
2014-03-06 12:21:26 +0100

    completed header, added submit form, added location picking (auto/manual)

--- index.php
+++ index.php
@@ -1,28 +1,60 @@
+<?
+$conn = mysql_connect("TOP", "SECRET", "DATA");
+mysql_select_db($conn, "DITO");
+?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 	<head>
 		<meta http-equiv="content-type" content="text/html" charset="UTF-8" />
+		<meta name="viewport" content="width=device-width, user-scalable=no">
 		<title>#WaitingForEd - map</title>
-		<link rel="SHORTCUT ICON" href="icon.ico">
+		<link rel="SHORTCUT ICON" href="icon.ico"/>
 		<link href="styles.css" rel="stylesheet" type="text/css"/>
 		<script src="openlayers/OpenLayers.js"></script>
+		<script src="jquery/js/jquery-1.10.2.js"></script>
+		<script src="jquery/js/jquery-ui-1.10.4.custom.js"></script>
+		<script src="jquery/js/jquery-ui-1.10.4.custom.min.js"></script>
 		<script type='text/javascript' src='map.js'></script>
 		<script type='text/javascript'>
 		</script>
 	</head>
 	<body onload='initialize_map();'>
+<!--	HEADER		-->
 		<div id='header'>
 			<span id='submits_nr'>
-				1.283
+				<? echo number_format(mysql_num_rows(mysql_query("SELECT * FROM submits"))); ?>
 			</span>
 			<span style='width:30px;'>
 				WAITING FOR ED
 			</span>
 		</div>
-		<div id='map'>
-		</div>
+		<div id='logo'></div>
+    	<div id="submit_plus" onclick='showhideform();'></div>
+    	
+<!--	FORM		-->	
+    	<form id='submit_form'>
+			<input type='text' placeholder='Alias' required/>
+			<input type='file' required/>
+			<input type='file'/>
+			<input type='file'/>
+			<textarea placeholder='Your message to the world. (optional)'></textarea>
+			<input type='button' value='SET
+LOCATION' id="set_location_button" onclick='map.events.register("click", map, man_set_user_location); this.style.color = "black"; this.style.backgroundColor = "white"; document.getElementById("map").style.cursor = "crosshair";'> <input type='button' value="AUTO
+DETECT" id="auto_location_button" onclick='if(navigator.geolocation){navigator.geolocation.getCurrentPosition(auto_set_user_location, location_error, {timeout:2000}); this.style.color = "black"; this.style.backgroundColor = "white";}else{alert("Your browser does not support geolocation.");}'>
+			<span id='location_set'>&#10008;</span>
+			<input type='reset'>
+			<input type='submit'>
+		</form>
+		
+<!--	MAP			-->	
+		<div id='map'></div>
+		
+<!--	NOSCRIPT	-->
 		<noscript>
 			Please activate javascript to properly display this webpage.
 		</noscript>
 	</body>
 </html>
+<?
+mysql_close($conn);
+?>
--- map.js
+++ map.js
@@ -1,17 +1,51 @@
 var user_lat = 0;
 var user_lon = 0;
-var map = '';
+var map;
+var markers = new OpenLayers.Layer.Markers( "Markers" );
+var user_location_marker;
 var map_e = '';
+var user_location_icon = new OpenLayers.Icon('location.png', new OpenLayers.Size(40,40), new OpenLayers.Pixel(-20, -40));
 
-function set_user_location(pos)
+function auto_set_user_location(pos)
 {
-		user_lat = Math.round(pos.coords.latitude);
-		user_lon = Math.round(pos.coords.longitude);
-		var fromProjection = new OpenLayers.Projection("EPSG:4326");   // Transform from WGS 1984
-        var toProjection   = new OpenLayers.Projection("EPSG:900913"); // to Spherical Mercator Projection
-        var position       = new OpenLayers.LonLat(user_lon,user_lat).transform( fromProjection, toProjection);
-        var zoom           = 3; 
-        map.setCenter(position, zoom);
+	if(user_lat != 0 && user_lon != 0)
+	{
+		markers.removeMarker(user_location_marker);
+	}
+	user_lat = pos.coords.latitude;
+	user_lon = pos.coords.longitude;
+	var fromProjection = new OpenLayers.Projection("EPSG:4326");   // Transform from WGS 1984
+	var toProjection   = new OpenLayers.Projection("EPSG:900913"); // to Spherical Mercator Projection
+	var position       = new OpenLayers.LonLat(user_lon,user_lat).transform( fromProjection, toProjection);
+	user_location_marker = new OpenLayers.Marker(position, user_location_icon);
+	markers.addMarker(user_location_marker);
+	user_lat = parseFloat(user_location_marker.lonlat.lat);
+	user_lon = parseFloat(user_location_marker.lonlat.lon);
+	map.setCenter(position, 3);
+	document.getElementById('location_set').style.color = "#00CC00";
+	document.getElementById('location_set').innerHTML = "&#10004;";
+	document.getElementById('auto_location_button').style.color = "white";
+	document.getElementById('auto_location_button').style.backgroundColor = "black";
+}
+
+function man_set_user_location()
+{
+	if(user_lat != 0 && user_lon != 0)
+	{
+		markers.removeMarker(user_location_marker);
+	}
+	var position = document.getElementsByClassName('olControlMousePosition')[0].innerHTML.split(";");
+	user_lat = parseFloat(position[1]);
+	user_lon = parseFloat(position[0]);
+	document.getElementById('location_set').style.color = "#00CC00";
+	document.getElementById('location_set').innerHTML = "&#10004;";
+	document.getElementById('set_location_button').style.color = "white";
+	document.getElementById('set_location_button').style.backgroundColor = "black";
+	document.getElementById("map").style.cursor = "default";
+	user_location_marker = new OpenLayers.Marker(new OpenLayers.LonLat(user_lon,user_lat), user_location_icon);
+	markers.addMarker(user_location_marker);
+	map.setCenter(new OpenLayers.LonLat(user_lon,user_lat), 3);
+	map.events.unregister("click", map, man_set_user_location);
 }
 
 function location_error(e)
@@ -19,29 +53,41 @@ function location_error(e)
 	if(e.code == 2 || e.code == 3)
 	{
 		alert("Your location can't be determined.");
+		document.getElementById('auto_location_button').style.color = "white";
+		document.getElementById('auto_location_button').style.backgroundColor = "black";
+	}
+}
+
+function showhideform()
+{
+	if(document.getElementById("submit_form").style.display != "block")
+	{
+		$("#submit_form").show("slide", {direction:"up", easing: "easeOutBounce"}, 500);
 	}
 	else
 	{
-		alert("Please activate location-services for the best experience.");
+		$("#submit_form").hide("slide", {direction:"up", easing: "easeOutQuart"}, 500);
 	}
 }
 
 function initialize_map()
 {
 	map = new OpenLayers.Map("map");
-	if(navigator.geolocation)
-	{
-		navigator.geolocation.getCurrentPosition(set_user_location, location_error, {timeout:2000});
-	}
         var mapnik         = new OpenLayers.Layer.OSM();
         var fromProjection = new OpenLayers.Projection("EPSG:4326");   // Transform from WGS 1984
         var toProjection   = new OpenLayers.Projection("EPSG:900913"); // to Spherical Mercator Projection
         var position       = new OpenLayers.LonLat(user_lon,user_lat).transform( fromProjection, toProjection);
         var zoom           = 0; 
     map.addControl(new OpenLayers.Control.PanZoomBar());
+    map.addControl(
+                new OpenLayers.Control.MousePosition({
+                    autoActivae: true,
+                    separator: ';',
+                    numDigits: 9,
+                    emptyString: 'out'
+                })
+            );
     map.addLayer(mapnik);
+    map.addLayer(markers);
     map.setCenter(position, zoom);
-//     document.getElementById("OpenLayers_Control_PanZoomBar_20").style.top = "50%";
-//     document.getElementById("OpenLayers_Control_Attribution_7").style.right = "5%";
-//     document.getElementById("OpenLayers_Control_Attribution_7").style.bottom = "5%";
 }
